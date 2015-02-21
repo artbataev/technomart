e1cc9d814cfc9777136a82897ad3bbcb7c16dbc0
artbataev <artbataev@gmail.com>
2015-02-21 22:09:23 +0300

    Добавлен js для модальных окон

--- js/script.js
+++ js/script.js
@@ -21,3 +21,54 @@ for (i = 0; i < links.length; i++) {
         }
     })
 }
+
+var emailLink = document.querySelector(".js-email"); 
+if(emailLink) {
+    var emailPopup = document.querySelector(".modal-content-email"); 
+    var emailClose = emailPopup.querySelector(".modal-content-close"); 
+    emailLink.addEventListener("click", function(event) { 
+        event.preventDefault(); 
+        emailPopup.classList.add("active"); 
+    }); 
+
+    emailClose.addEventListener("click", function(event) {
+        event.preventDefault();
+        emailPopup.classList.remove("active");
+    });
+
+    window.addEventListener("keydown", function(event) {
+        if (event.keyCode == 27) { 
+            if (emailPopup.classList.contains("active")) { 
+                emailPopup.classList.remove("active"); 
+            } 
+        }
+    });
+}
+
+var addToCartLinks = document.querySelectorAll(".js-addtocart");
+if(addToCartLinks) {
+    var addToCartPopup = document.querySelector(".modal-content-addtocart"); 
+    var addToCartClose = addToCartPopup.querySelector(".modal-content-close"); 
+    for (var j = 0; j < addToCartLinks.length; j++) {
+        addToCartLinks[j].addEventListener("click", function(event) {
+            event.preventDefault();
+            addToCartPopup.classList.add("active");
+        });
+    }
+
+    addToCartClose.addEventListener("click", function(event) {
+        event.preventDefault();
+        addToCartPopup.classList.remove("active");
+    });
+
+    window.addEventListener("keydown", function(event) {
+        if (event.keyCode == 27) { 
+            if (addToCartPopup.classList.contains("active")) { 
+                addToCartPopup.classList.remove("active"); 
+            } 
+        }
+    });
+}
+
+
+
